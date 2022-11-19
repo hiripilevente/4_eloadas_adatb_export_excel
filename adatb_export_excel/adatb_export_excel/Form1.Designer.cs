571ef6a6301c4196c2625ca525971cacb32ea760
hiripilevente <hiripilevi@gmail.com>
2022-11-19 16:39:25 +0100

    feladat2

--- adatb_export_excel/adatb_export_excel/Form1.Designer.cs
+++ adatb_export_excel/adatb_export_excel/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace adatb_export_excel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(899, 463);
+            this.ClientSize = new System.Drawing.Size(805, 446);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
