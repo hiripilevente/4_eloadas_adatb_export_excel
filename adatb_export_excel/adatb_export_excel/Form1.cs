19ff00c6fe5b7ffe08fdb2c0d98fc7e5568ce82a
hiripilevente <hiripilevi@gmail.com>
2022-11-19 17:42:59 +0100

    feladat6

--- adatb_export_excel/adatb_export_excel/Form1.cs
+++ adatb_export_excel/adatb_export_excel/Form1.cs
@@ -52,13 +52,46 @@ namespace adatb_export_excel
 
         private void CreateTable()
         {
-            
+       
+         string[] headers = new string[] {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            foreach (var f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
 
         }
     }
