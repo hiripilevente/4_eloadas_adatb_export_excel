e56bd6eca76b02c6ce28ca695f2675cf60b83dae
hiripilevente <hiripilevi@gmail.com>
2022-11-19 17:30:48 +0100

    feladat6

--- adatb_export_excel/adatb_export_excel/Form1.cs
+++ adatb_export_excel/adatb_export_excel/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel; // ad neki egy nevet, az excel helyére lehetne teljesen mast írni
+using Excel = Microsoft.Office.Interop.Excel; // ad a fajlnak egy nevet, az excel helyére lehetne teljesen mast írni
 using System.Reflection;
 
 namespace adatb_export_excel
@@ -18,10 +18,43 @@ namespace adatb_export_excel
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
 
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         void LoadData()
         {
             Flats = context.Flat.ToList();
         }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add();
+                xlSheet = xlWB.ActiveSheet();
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Source + '\n' + ex.Message);
+                xlWB.Close(false);
+                xlApp.Quit();
+                xlApp = null;
+                xlWB = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            
+        }
+
         public Form1()
         {
             InitializeComponent();
